1455f91fd3b3071491a1c352e2f20947f42ad951
juhotalus <juho.talus@gmail.com>
2021-11-26 17:33:19 +0200

    Show status of all instances when instance is not specified

--- DiscordBot/src/Bot.js
+++ DiscordBot/src/Bot.js
@@ -28,7 +28,7 @@ const { CHANNELID } = require('../config.json');
 
 const PREFIX = '!aws';
 const HelpDocs =
-    '\n Help Docs: \n `start`: Starts the server \n `stop`: Stops the server \n `status`: Returns the server status  ';
+  '\n Help Docs: \n `start`: Starts the server \n `stop`: Stops the server \n `status`: Returns the server status  ';
 
 
 // Create a Client instance with our bot token.
@@ -36,144 +36,147 @@ const bot = new eris.Client(BOT_TOKEN);
 
 // When the bot is connected and ready, log to console.
 bot.on('ready', () => {
-    console.log('Connected and ready.');
+  console.log('Connected and ready.');
 });
 
 const commandHandlerForCommandName = {};
 commandHandlerForCommandName['start'] = (msg, args) => {
-    startStop(true, msg, args);
+  startStop(true, msg, args);
 };
 
 commandHandlerForCommandName['stop'] = (msg, args) => {
-    startStop(false, msg, args);
+  startStop(false, msg, args);
 };
 
 commandHandlerForCommandName['help'] = (msg, args) => {
-    return msg.channel.createMessage(HelpDocs);
+  return msg.channel.createMessage(HelpDocs);
 };
 
-function startStop(isStart, msg, args){
-	var cmd = isStart ? StartCommand : StopCommand;
-        cmd = cmd + INSTANCES[args.commandArg];
-	var word = isStart ? 'Starting' : 'Stopping';
-	console.warn(`${word} the server`);
-	try {
-        aws.command(cmd)
-            .then(function (data) {
-                console.warn('data = ', data);
-                return msg.channel.createMessage(`<@${msg.author.id}> ${word} the ${args.commandArg} server`);
-            })
-            .catch(function (e) {
-                if (verboseLog) {
-                    msg.channel.createMessage(`<@${msg.author.id}> Error ${word} the ${args.commandArg} server,  ${e}`);
-                } else {
-                    msg.channel.createMessage(`<@${msg.author.id}> Error ${word} the ${args.commandArg} server`);
-                }
-            });
+function startStop(isStart, msg, args) {
+  var cmd = isStart ? StartCommand : StopCommand;
+  cmd = cmd + INSTANCES[args.commandArg];
+  var word = isStart ? 'Starting' : 'Stopping';
+  console.warn(`${word} the server`);
+  try {
+    aws.command(cmd)
+      .then(function (data) {
+        console.warn('data = ', data);
+        return msg.channel.createMessage(`<@${msg.author.id}> ${word} the ${args.commandArg} server`);
+      })
+      .catch(function (e) {
+        if (verboseLog) {
+          msg.channel.createMessage(`<@${msg.author.id}> Error ${word} the ${args.commandArg} server,  ${e}`);
+        } else {
+          msg.channel.createMessage(`<@${msg.author.id}> Error ${word} the ${args.commandArg} server`);
+        }
+      });
 
-    } catch (err) {
-        msg.channel.createMessage(`<@${msg.author.id}> Error ${word} the ${args.commandArg} server`);
-        return msg.channel.createMessage(err);
-    }
+  } catch (err) {
+    msg.channel.createMessage(`<@${msg.author.id}> Error ${word} the ${args.commandArg} server`);
+    return msg.channel.createMessage(err);
+  }
 }
 
 commandHandlerForCommandName['status'] = (msg, args) => {
-    console.warn("Getting server status");
-    try {
-        aws.command(StatusCommand + INSTANCES[args.commandArg])
-            .then(function (data) {
-		var reply = data.object.Reservations[0].Instances[0];
-                return msg.channel.createMessage(`<@${msg.author.id}> *Status: \n **Name**: ${reply.Tags[0].Value} \n **State**: ${reply.State.Name} \n **IP Address**: ${reply.PublicIpAddress} \n **Last Startup**: ${reply.LaunchTime}*`);
-            })
-            .catch(function (e) {
-                if (verboseLog) {
-                    msg.channel.createMessage(`<@${msg.author.id}> Error getting status,  ${e}`);
-                } else {
-                    msg.channel.createMessage(`<@${msg.author.id}> Error getting status`);
-                }
-		console.warn(e);
-            });
+  console.warn("Getting server status");
+  try {
+    aws.command(StatusCommand + args.commandArg ? INSTANCES[args.commandArg] : null)
+      .then(function (data) {
+        data.object.Reservations[0].Instances.forEach(instance => {
+          return msg.channel.createMessage(`*Status: \n **Name**: ${instance.Tags[0].Value} \n **State**: ${instance.State.Name} \n **IP Address**: ${instance.PublicIpAddress} \n **Last Startup**: ${instance.LaunchTime}*`);
+        });
+      })
+      .catch(function (e) {
+        if (verboseLog) {
+          msg.channel.createMessage(`<@${msg.author.id}> Error getting status,  ${e}`);
+        } else {
+          msg.channel.createMessage(`<@${msg.author.id}> Error getting status`);
+        }
+        console.warn(e);
+      });
 
-    } catch (err) {
-	console.warn(err);
-        msg.channel.createMessage(`<@${msg.author.id}> Error getting status`);
-        return msg.channel.createMessage(err);
-    }
+  } catch (err) {
+    console.warn(err);
+    msg.channel.createMessage(`<@${msg.author.id}> Error getting status`);
+    return msg.channel.createMessage(err);
+  }
 };
 
 // Every time a message is sent anywhere the bot is present,
 // this event will fire and we will check if the bot was mentioned.
 // If it was, the bot will attempt to respond with "Present".
 bot.on('messageCreate', async (msg) => {
-    const content = msg.content;
-    const botWasMentioned = msg.mentions.find(
-        mentionedUser => mentionedUser.id === bot.user.id,
-    );
+  const content = msg.content;
+  const botWasMentioned = msg.mentions.find(
+    mentionedUser => mentionedUser.id === bot.user.id,
+  );
 
-    //make sure it's in the right channel
-    if (!(msg.channel.id === CHANNELID)) {
-        return;
-    }
+  //make sure it's in the right channel
+  if (!(msg.channel.id === CHANNELID)) {
+    return;
+  }
 
-    //if the message is sent by a bot, ignore it
-    if (msg.author.bot) {
-        return;
-    }
+  //if the message is sent by a bot, ignore it
+  if (msg.author.bot) {
+    return;
+  }
 
-    if (!msg.member.roles.includes(ROLEID)) {
-        await msg.channel.createMessage(`<@${msg.author.id}> You do not have the required roles`);
-        return;
-    }
+  if (!msg.member.roles.includes(ROLEID)) {
+    await msg.channel.createMessage(`<@${msg.author.id}> You do not have the required roles`);
+    return;
+  }
 
-    if (botWasMentioned) {
-        await msg.channel.createMessage('Brewing the coffee and ready to go!');
-    }
+  if (botWasMentioned) {
+    await msg.channel.createMessage('Brewing the coffee and ready to go!');
+  }
 
-    //ignore dms, guild messages only
-    if (!msg.channel.guild) {
-        console.warn('Received a dm, ignoring');
-        return;
-    }
+  //ignore dms, guild messages only
+  if (!msg.channel.guild) {
+    console.warn('Received a dm, ignoring');
+    return;
+  }
 
-    // Ignore any message that doesn't start with the correct prefix.
-    if (!content.startsWith(PREFIX)) {
-        return;
-    }
-    // Extract the parts of the command and the command name
-    const command = content.split(PREFIX)[1].trim().split(' ');
-    const commandName = command[0] ? command[0].trim() : undefined;
-    const commandArg = command[1] ? command[1].trim() : undefined;
+  // Ignore any message that doesn't start with the correct prefix.
+  if (!content.startsWith(PREFIX)) {
+    return;
+  }
+  // Extract the parts of the command and the command name
+  const command = content.split(PREFIX)[1].trim().split(' ');
+  const commandName = command[0] ? command[0].trim() : undefined;
+  const commandArg = command[1] ? command[1].trim() : undefined;
 
-    // Get the appropriate handler for the command, if there is one.
-    const commandHandler = commandHandlerForCommandName[commandName];
-    if (!commandHandler) {
-        await msg.channel.createMessage('Unkown command, try `$aws help` for a list of commands');
-        return;
-    }
+  // Get the appropriate handler for the command, if there is one.
+  const commandHandler = commandHandlerForCommandName[commandName];
+  if (!commandHandler) {
+    await msg.channel.createMessage('Unkown command, try `$aws help` for a list of commands');
+    return;
+  }
 
+  if (commandName !== 'status') {
     if (!commandArg) {
-        await msg.channel.createMessage('Please specify the instance as an argument.');
-        return;
+      await msg.channel.createMessage('Please specify the instance as an argument.');
+      return;
     }
 
     if (!INSTANCES[commandArg]) {
-        await msg.channel.createMessage('Unknown instance.');
-        return;
+      await msg.channel.createMessage('Unknown instance.');
+      return;
     }
+  }
 
-    // Separate the command arguments from the command prefix and command name.
+  // Separate the command arguments from the command prefix and command name.
 
-    try {
-        // Execute the command.
-        await commandHandler(msg, { commandName, commandArg });
-    } catch (err) {
-        console.warn('Error handling command');
-        console.warn(err);
-    }
+  try {
+    // Execute the command.
+    await commandHandler(msg, { commandName, commandArg });
+  } catch (err) {
+    console.warn('Error handling command');
+    console.warn(err);
+  }
 });
 
 bot.on('error', err => {
-    console.warn(err);
+  console.warn(err);
 });
 
 bot.connect();
